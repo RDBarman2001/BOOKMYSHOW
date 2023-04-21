11964014c2567fa1010ecbd6c2751a728d859755
Rupak Deb Barman <71335076+RDBarman2001@users.noreply.github.com>
2023-04-21 14:57:07 +0530

    Update MovieInfo.Component.jsx

--- src/components/MovieHero/MovieInfo.Component.jsx
+++ src/components/MovieHero/MovieInfo.Component.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import PaymentModel from "../PaymentModal/Payment.Component";
 import { MovieContext } from "../../context/Movie.context";
 
-let priceis = Math.floor(Math.random() * 100);
+ Math.floor(Math.random() * 100);
 
 const MovieInfo = ({ movie }) => {
     const { price, setIsOpen, isOpen, rentMovie, buyMovie } =
@@ -25,9 +25,12 @@ const MovieInfo = ({ movie }) => {
                 <div className="flex items-center gap-3 w-full">
                     <button
                         onClick={rentMovie}
-                        className="bg-red-500 price w-full py-3 text-white font-semibold rounded-lg"
-                    >
-                       priceis
+                        className="bg-red-500 price w-full py-3 text-white font-semibold rounded-lg">
+                            <script>
+                            Math.floor(Math.random() * 100); 
+
+                            </script> 
+                      
                     </button>
                     <button
                         onClick={buyMovie}
